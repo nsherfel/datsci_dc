9360dff6175c57b796a08a762f14921c33e7c63d
nsherfel <59976220+nsherfel@users.noreply.github.com>
2024-05-07 23:38:19 -0500

    Update catalog_generator.py
    swapped to use the new get_examples bs

--- data_catalog/catalog_generator.py
+++ data_catalog/catalog_generator.py
@@ -12,17 +12,23 @@ def generate_data_catalog(df, path_to_yaml=None, output_type='csv'):
 
     catalog = []
     for column in df.columns:
-        example_values = get_example_values(df[column])
-        column_info = get_column_info(df[column])
+        column_info = get_example_values(df[column])
         data = {
             "Field Name": column,
             "Data Type": str(df[column].dtype),
             "Source": definitions.get(column, {}).get("source", "TBD"),
             "Definition": definitions.get(column, {}).get("definition", "No definition provided"),
-            "Example Values": example_values,
-            "Percent Null": f"{df[column].isnull().mean() * 100:.2f}%",
-            "Statistics": column_info
+            "Example Values": column_info['examples'],
+            "Percent Null": f"{df[column].isnull().mean() * 100:.2f}%"
         }
+        
+        # Combining range and categories into a single Statistics field
+        stats_details = []
+        if 'categories' in column_info:
+            stats_details.append(f"Categories: {column_info['categories']}")
+        if 'range' in column_info:
+            stats_details.append(f"Range: {column_info['range'][0]} to {column_info['range'][1]}")
+        data["Statistics"] = ', '.join(stats_details)
         
         catalog.append(data)
     
@@ -34,15 +40,3 @@ def generate_data_catalog(df, path_to_yaml=None, output_type='csv'):
         return catalog_df.to_csv(index=False)
     else:
         raise ValueError("Unsupported output type. Use 'markdown' or 'csv'.")
-
-    
-    catalog_df = pd.DataFrame(catalog)
-
-    if output_type == 'markdown':
-        return catalog_df.to_markdown(index=False)
-    elif output_type == 'csv':
-        return catalog_df.to_csv(index=False)
-    else:
-        raise ValueError("Unsupported output type. Use 'markdown' or 'csv'.")
-
-
