b086f2df7b0aec6ace3f81763931cd2db86326d3
nsherfel <59976220+nsherfel@users.noreply.github.com>
2024-05-07 23:42:22 -0500

    Update __init__.py
    changed from run_server to edit_definitions

--- data_catalog/__init__.py
+++ data_catalog/__init__.py
@@ -1,3 +1,3 @@
 from .catalog_generator import generate_data_catalog
-from .dash_app import run_server
+from .dash_app import edit_definitions
 from .utils import load_definitions, generate_initial_yaml
