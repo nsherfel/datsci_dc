c06d3322ebddc00a0e5d7f2592091a2193a6d057
nsherfel <59976220+nsherfel@users.noreply.github.com>
2024-05-07 22:37:09 -0500

    Initial Commit
    Adding all package files.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data_catalog',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'dash',
+        'plotly',
+        'dash-bootstrap-components', 
+        'pyyaml'
+    ],
+    entry_points={
+        'console_scripts': [
+            'data-catalog = data_catalog.dash_app:run_server'
+        ]
+    },
+    author='Nic Herfel',
+    author_email='nsherfel@gmail.com',
+    description='An interactive data cataloging tool for internal use.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='[]',
+)
