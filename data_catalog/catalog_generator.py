94b083e6a3ee68c6e10dd8395be94d3a85806f4c
nsherfel <59976220+nsherfel@users.noreply.github.com>
2024-10-02 18:07:04 -0500

    Update catalog_generator.py

--- data_catalog/catalog_generator.py
+++ data_catalog/catalog_generator.py
@@ -13,21 +13,40 @@ def generate_data_catalog(df, path_to_yaml=None, output_type='df'):
     catalog = []
     
     for column in df.columns:
-        column_info = get_example_values(df[column])
-        data = {
-            "Field Name": column,
-            "Data Type": str(df[column].dtype),
-            "Source": definitions.get(column, {}).get("source", "TBD"),
-            "Definition": definitions.get(column, {}).get("definition", "No definition provided"),
-            "Example Values": column_info['examples'],
-            "Percent Null": f"{df[column].isnull().mean() * 100:.2f}%",
-            "Statistics": ', '.join([f"{k}: {', '.join(map(str, v))}" if isinstance(v, tuple) else f"{k}: {v}" for k, v in column_info.items() if k != 'examples']),
-            "Status": definitions.get(column, {}).get("status", "added")
-        }
+        existing_data = definitions.get(column, {})
+        status = existing_data.get("status", "added")
+        
+        if status == "added":
+            # Update statistics only for 'added' fields
+            column_info = get_example_values(df[column])
+            data = {
+                "Field Name": column,
+                "Data Type": str(df[column].dtype),
+                "Source": existing_data.get("source", "TBD"),
+                "Definition": existing_data.get("definition", "No definition provided"),
+                "Example Values": column_info['examples'],
+                "Percent Null": f"{df[column].isnull().mean() * 100:.2f}%",
+                "Statistics": ', '.join([f"{k}: {', '.join(map(str, v))}" if isinstance(v, tuple) else f"{k}: {v}" for k, v in column_info.items() if k != 'examples']),
+                "Status": status
+            }
+        else:
+            # For non-'added' fields, use existing data without updating statistics
+            data = {
+                "Field Name": column,
+                "Data Type": existing_data.get("data type", str(df[column].dtype)),
+                "Source": existing_data.get("source", "TBD"),
+                "Definition": existing_data.get("definition", "No definition provided"),
+                "Example Values": existing_data.get("example values", "N/A"),
+                "Percent Null": existing_data.get("percent null", "N/A"),
+                "Statistics": existing_data.get("statistics", "N/A"),
+                "Status": status
+            }
+        
         # Add any additional custom fields from the YAML file
-        for key, value in definitions.get(column, {}).items():
+        for key, value in existing_data.items():
             if key.lower() not in [k.lower() for k in data.keys()]:
                 data[key] = value
+        
         catalog.append(data)
     
     catalog_df = pd.DataFrame(catalog)
