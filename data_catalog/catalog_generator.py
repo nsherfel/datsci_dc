d12b5116aac954cbee43d15265bf4fe664176c1a
nsherfel <59976220+nsherfel@users.noreply.github.com>
2024-10-02 15:54:37 -0500

    Update catalog_generator.py

--- data_catalog/catalog_generator.py
+++ data_catalog/catalog_generator.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import os
-from .utils import load_definitions, get_example_values, generate_initial_yaml
+from utils import load_definitions, get_example_values, generate_initial_yaml
 
 def generate_data_catalog(df, path_to_yaml=None, output_type='df'):
     if path_to_yaml and not os.path.exists(path_to_yaml):
@@ -8,10 +7,10 @@ def generate_data_catalog(df, path_to_yaml=None, output_type='df'):
     elif not path_to_yaml:
         path_to_yaml = 'data_definitions.yaml'
         generate_initial_yaml(df, path_to_yaml)
-
+    
     definitions = load_definitions(path_to_yaml)
-
     catalog = []
+    
     for column in df.columns:
         column_info = get_example_values(df[column])
         data = {
@@ -21,19 +20,18 @@ def generate_data_catalog(df, path_to_yaml=None, output_type='df'):
             "Definition": definitions.get(column, {}).get("definition", "No definition provided"),
             "Example Values": column_info['examples'],
             "Percent Null": f"{df[column].isnull().mean() * 100:.2f}%",
-            "Statistics": ', '.join([f"{k}: {', '.join(map(str, v))}" if isinstance(v, tuple) else f"{k}: {v}" for k, v in column_info.items() if k != 'examples'])        }
+            "Statistics": ', '.join([f"{k}: {', '.join(map(str, v))}" if isinstance(v, tuple) else f"{k}: {v}" for k, v in column_info.items() if k != 'examples']),
+            "Status": definitions.get(column, {}).get("status", "added")
+        }
         catalog.append(data)
     
     catalog_df = pd.DataFrame(catalog)
-
-    # Ensure that the DataFrame is returned by default
+    
     if output_type == 'markdown':
         return catalog_df.to_markdown(index=False)
     elif output_type == 'csv':
         return catalog_df.to_csv(index=False)
     elif output_type == 'df':
-        return catalog_df  # This line ensures a DataFrame is returned
+        return catalog_df
     else:
         raise ValueError("Unsupported output type. Use 'markdown', 'csv', or 'df'.")
-
-
